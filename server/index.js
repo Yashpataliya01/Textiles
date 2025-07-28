372f76fd9e1baba92ef39ea30ebdbf5058e050a5
yashpataliya01 <yashpataliya01@gmail.com>
2025-07-28 16:54:02 +0530

    male chng

--- server/index.js
+++ server/index.js
@@ -51,10 +51,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-export const api = functions
-  .region("us-central1") // or change to asia-south1, europe-west1, etc.
-  .https.onRequest(app);
-
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
